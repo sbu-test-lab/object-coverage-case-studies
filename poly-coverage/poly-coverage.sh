66d7493bef3899d3414a5e2535a3580c7997f463
mgh <m.ghoreshi@gmail.com>
2021-03-16 18:42:13 +0330

    poly coverage improvment

--- poly-coverage/poly-coverage.sh
+++ poly-coverage/poly-coverage.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar ../tools-jars/ocov/ocov.jar --poly -a ../jtetris-Block/oocov-coverable-lines.csv org.jtetris.models.elements.Block
+java -jar ../tools-jars/ocov/ocov.jar --poly -a ../jtetris-Rectangle/oocov-coverable-lines.csv org.jtetris.models.elements.Grid
